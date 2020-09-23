16750e40a6a7cf236f57d501a3355c84a3bd9dbb
Weitang Li <liwt31@163.com>
2020-09-23 19:11:12 +0800

    finite temperature first try

--- renormalizer/mps/tests/test_umps.py
+++ renormalizer/mps/tests/test_umps.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import math
+
+import pytest
 
 from renormalizer.model import UniformModel, Op
 from renormalizer.model.basis import BasisHalfSpin, BasisSimpleElectron, BasisSHO
@@ -35,16 +38,27 @@ def test_ising_2site():
         print(mps.expectation(mpo)/2)
 
 
+def test_harmonic_oscillator():
+    omega = 1
+    beta = 1
+    basis = [BasisSHO(0, omega, 20), BasisSHO(1, omega, 20)]
+    ham_terms = [Op(r"b^\dagger b", 0, omega)]
+    model = UniformModel(basis, ham_terms)
+    mps = UMPS.identity(model, 2)
+    mpo = Mpo(model)
+    mps = mps.evolve(mpo, -beta/2)
+    assert pytest.approx(1/(math.e-1), mps.expectation(Mpo(model, Op(r"b^\dagger b", 0))))
+
+
 def hueckel():
     set_stream_level(logging.INFO)
     J = 1
-
+    epsilon = 1
     basis = [BasisSimpleElectron("e0"), BasisSimpleElectron("e1")]
-    ham_terms = [Op(r"a^\dagger a", ["e0", "e1"], -J), Op(r"a a^\dagger", ["e0", "e1"], -J)]
+    ham_terms = [Op(r"a^\dagger a", ["e0", "e1"], -J), Op(r"a^\dagger a", ["e1", "e0"], -J), Op(r"a^\dagger a", "e0", epsilon)]
+    #ham_terms = [Op(r"a^\dagger a", "e0", epsilon)]
     model = UniformModel(basis, ham_terms)
-    mps = UMPS.random_with_qn(model, 2)
-    mps[0][mps[0] != 0] = 1
-    mps.normalize()
+    mps = UMPS.identity(model, 1)
     mpo = Mpo(model)
     print(mps.expectation(mpo), mps.expectation(Mpo(model, Op("a^\dagger a", "e0"))))
     for i in range(10):
@@ -54,19 +68,20 @@ def hueckel():
 def holstein():
     set_stream_level(logging.INFO)
     J = 1
+    epsilon = 1
     lam = 1
     omega = 1
     nbas = 2
     basis = [BasisSimpleElectron("e0"), BasisSHO("ph0", omega, nbas), BasisSimpleElectron("e1"), BasisSHO("ph1", omega, nbas)]
-    ham_terms = [Op(r"a^\dagger a", ["e0", "e1"], -J), Op(r"a a^\dagger", ["e0", "e1"], -J)]
+    ham_terms = [Op(r"a^\dagger a", ["e0", "e1"], -J), Op(r"a^\dagger a", ["e1", "e0"], -J), Op(r"a^\dagger a", "e0", epsilon)]
     ham_terms.append(Op(r"b^\dagger b", "ph0", omega))
     ham_terms.append(Op(r"a^\dagger a", "e0") * Op(r"b^\dagger + b", "ph0", -lam))
     model = UniformModel(basis, ham_terms)
-    mps = UMPS.random_with_qn(model, 4)
+    mps = UMPS.identity(model, 4)
     mpo = Mpo(model)
     print(mps.expectation(mpo), mps.expectation(Mpo(model, Op("b^\dagger b", "ph0"))))
     for i in range(10):
         mps = mps.evolve(mpo, -1)
         print(mps.expectation(mpo), mps.expectation(Mpo(model, Op("b^\dagger b", "ph0"))))
 
-hueckel()
+test_ising()
--- renormalizer/mps/umps.py
+++ renormalizer/mps/umps.py
@@ -21,22 +21,24 @@ class UMPS:
         mps._mp = []
         np.random.seed(2021)
         for basis in model.basis[:model.unitcell_size]:
-            mps._mp.append(np.random.rand(bond_dim, basis.nbas, bond_dim) - 0.5)
+            mt = np.random.rand(bond_dim, basis.nbas, bond_dim) - 0.5
+            mps._mp.append(mt)
         mps.normalize()
         return mps
 
     @classmethod
-    def random_with_qn(cls, model: UniformModel, bond_dim: int):
-        # qntot is assumed to be 1. QN is half 0, half 1
-        qn = np.ones(bond_dim)
-        qn[:bond_dim//2] = 0
+    def identity(cls, model: UniformModel, bond_dim: int):
         mps = cls(model)
         mps._mp = []
         np.random.seed(2021)
         for basis in model.basis[:model.unitcell_size]:
-            mt = np.random.rand(bond_dim, basis.nbas, bond_dim) - 0.5
-            zero_indices = np.subtract.outer(np.add.outer(qn, basis.sigmaqn), qn) != 0
-            mt[zero_indices] = 0
+            """
+            mt = np.zeros((bond_dim, basis.nbas, basis.nbas, bond_dim))
+            mt[0, :, :, 0] = np.eye(basis.nbas)
+            mt[1:, :, :, 1:] = np.random.rand(bond_dim-1, basis.nbas, basis.nbas, bond_dim-1)
+            """
+            mt = mps.regular_epsilon * np.random.rand(bond_dim, basis.nbas, basis.nbas, bond_dim)
+            mt[0, :, :, 0] = np.eye(basis.nbas)
             mps._mp.append(mt)
         mps.normalize()
         return mps
@@ -56,6 +58,7 @@ class UMPS:
         self.model: UniformModel = model
         self.l = None
         self.r = None
+        self.regular_epsilon = 1e-3
 
     @property
     def boundary_bond_dim(self):
@@ -70,14 +73,21 @@ class UMPS:
         def E_oper_func(x):
             x = x.reshape(M, M)
             """
-            - ms - ms - |
-               |    |   x
             - ms'- ms'- |
+               |    |   x
+            - ms - ms - |
             """
             argument_list = []
             for i, mt in enumerate(mps):
-                # top, middle, bottom
-                argument_list.extend([mt, (f"t{i}", f"m{i}", f"t{i+1}"), mt.conj(), (f"b{i}", f"m{i}", f"b{i+1}")])
+                if mt.ndim == 3:
+                    # top, middle, bottom
+                    args = [mt.conj(), (f"t{i}", f"m{i}", f"t{i+1}"), mt, (f"b{i}", f"m{i}", f"b{i+1}")]
+                elif mt.ndim == 4:
+                    # auxiliary, top, middle, bottom
+                    args = [mt.conj(), (f"t{i}", f"m{i}", f"a{i}",  f"t{i+1}"), mt, (f"b{i}", f"m{i}", f"a{i}", f"b{i+1}")]
+                else:
+                    assert False
+                argument_list.extend(args)
             argument_list.extend([x, (f"t{len(mps)}", f"b{len(mps)}"), ("t0", "b0")])
             return contract(*argument_list)
 
@@ -87,7 +97,11 @@ class UMPS:
         flipped_mps = self._metacopy()
         flipped_mps.l, flipped_mps.r = flipped_mps.r, flipped_mps.l
         for mt in reversed(self._mp):
-            flipped_mps.append(mt.transpose(2, 1, 0))
+            if mt.ndim == 3:
+                flipped_mps.append(mt.transpose(2, 1, 0))
+            else:
+                assert mt.ndim == 4
+                flipped_mps.append(mt.transpose(3, 1, 2, 0))
         return flipped_mps
 
     def _build_E_oper_T(self):
@@ -95,8 +109,13 @@ class UMPS:
 
     def normalize(self):
         # This function is extremely time consuming. Avoid normalization when possible
-        evals1, evecs1 = eigs(self._build_E_oper(), k=2, which="LM")
-        evals2, evecs2 = eigs(self._build_E_oper_T(), k=2, which="LM")
+        if self.boundary_bond_dim == 1:
+            # for test
+            evecs1 = evecs2 = np.array([[1.+0j]])
+            evals1 = evals2 = self._build_E_oper() @ evecs1[:, 0]
+        else:
+            evals1, evecs1 = eigs(self._build_E_oper(), k=1, which="LM")
+            evals2, evecs2 = eigs(self._build_E_oper_T(), k=1, which="LM")
         assert np.allclose(evals1, evals2)
         normalize_coef = np.sqrt(evals1[0] ** (1/len(self)))
         assert np.allclose(normalize_coef.imag, 0)
@@ -166,6 +185,12 @@ class UMPS:
         flipped_h_mpo = [mt.transpose(3, 1, 2, 0) for mt in reversed(h_mpo)]
         return self._flip()._solve_L_h(flipped_h_mpo)
 
+    def my_pinv(self, S):
+        u, s, vt = np.linalg.svd(S)
+        new_s = s + self.regular_epsilon
+        s_inv = np.diag(1 / new_s)
+        return vt.T.conj() @ s_inv @ u.T.conj()
+
     def calc_deriv(self, mpo):
         self._check_normalize()
         e = self.expectation(mpo)
@@ -191,36 +216,63 @@ class UMPS:
             # The first term.
             left_part = env_lh.read("L", idx - 1).squeeze(axis=1)
             right_part = env.read("R", idx + 1).squeeze(axis=1)
-            F1 = contract("ab, acd, de -> bce", left_part, mt, right_part)
+            """
+            | a      d|
+            l         r
+            |b   |c  e|
+             --- ms---  
+            """
+            if mt.ndim == 3:
+                F1 = contract("ab, bce, de -> acd", left_part, mt, right_part)
+            else:
+                assert mt.ndim == 4
+                F1 = contract("ab, bcfe, de -> acfd", left_part, mt, right_part)
             # The second term
             left_part = env_h.read("L", idx - 1)
             right_part = env_h.read("R", idx + 1)
             """
              a       f
-             -- ms --- 
+             --    --- 
             |  b |d g |
             l -- o -- r
             |c   |e  h|
-             ---   ---  
+             --- ms---  
             """
-            F2 = contract("abc, adf, bdeg, fgh -> ceh", left_part, mt, h_mpo[idx], right_part)
+            if mt.ndim == 3:
+                F2 = contract("abc, ceh, bdeg, fgh -> adf", left_part, mt, h_mpo[idx], right_part)
+            else:
+                assert mt.ndim == 4
+                F2 = contract("abc, ceih, bdeg, fgh -> adif", left_part, mt, h_mpo[idx], right_part)
             # The third term
             left_part = env_h.read("L", idx + nsite - 1)
             right_part = env_h.read("R", idx + nsite + 1)
-            F3 = contract("abc, adf, bdeg, fgh -> ceh", left_part, mt, h_mpo[idx + nsite], right_part)
+            if mt.ndim == 3:
+                F3 = contract("abc, ceh, bdeg, fgh -> adf", left_part, mt, h_mpo[idx + nsite], right_part)
+            else:
+                assert mt.ndim == 4
+                F3 = contract("abc, ceih, bdeg, fgh -> adif", left_part, mt, h_mpo[idx + nsite], right_part)
             # the fourth term
             left_part = env.read("L", idx - 1).squeeze(axis=1)
             right_part = env_rh.read("R", idx + 1).squeeze(axis=1)
-            F4 = contract("ab, acd, de -> bce", left_part, mt, right_part)
+            if mt.ndim == 3:
+                F4 = contract("ab, bce, de -> acd", left_part, mt, right_part)
+            else:
+                assert mt.ndim == 4
+                F4 = contract("ab, bcfe, de -> acfd", left_part, mt, right_part)
             F = F1 + F2 + F3 + F4
+            if mt.ndim == 4:
+                F = F.reshape(F.shape[0], d**2, F.shape[-1])
             l_half = scipy.linalg.sqrtm(env.read("L", idx - 1).squeeze(axis=1))
             V_L_target = np.tensordot(l_half, mt.conj(), axes=1).reshape(-1, M).T
             V_L = scipy.linalg.null_space(V_L_target)
-            # null space could be larger than expected because of the sparsity enforced by symmetry
             assert np.allclose(V_L.conj().T @ V_L, np.eye(V_L.shape[-1]))
-            V_L = V_L.reshape(M, d, -1)
-            l_half_inv = scipy.linalg.pinv(l_half)
-            r_inv = scipy.linalg.pinv(env.read("R", idx + 1).squeeze(axis=1))
+            if mt.ndim == 3:
+                V_L = V_L.reshape(M, d, -1)
+            else:
+                assert mt.ndim == 4
+                V_L = V_L.reshape(M, d**2, -1)
+            l_half_inv = self.my_pinv(l_half)
+            r_inv = self.my_pinv(env.read("R", idx + 1).squeeze(axis=1))
             deriv = contract(
                 "ab, ce, bdh, efh, adg, gi -> cfi",
                 l_half_inv,
@@ -242,6 +294,7 @@ class UMPS:
         self._check_normalize()
         init_y = np.concatenate([mt.ravel() for mt in self._mp])
         sol = solve_ivp(fun, (0, dt), init_y, method="RK45")
+        #sol = solve_ivp(fun, (0, dt), init_y, method="RK45", rtol=1e-5, atol=1e-8)
         return self.__class__.from_array(sol.y[:, -1], self)
 
 
